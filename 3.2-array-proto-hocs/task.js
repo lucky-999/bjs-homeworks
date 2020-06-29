90665089b45a3f6515bf8679e0354c72f877072e
lucky-999 <62803727+lucky-999@users.noreply.github.com>
2020-06-29 22:16:22 +0300

    HW done

---
+++ 3.2-array-proto-hocs/task.js
@@ -0,0 +1,34 @@
+'use strict'
+
+function sleep(milliseconds) {
+  let e = new Date().getTime() + milliseconds;
+  while (new Date().getTime() <= e) {}
+}
+
+function sum(...args) {
+  sleep(100);
+  return args.reduce((sum, arg) => {
+    return sum += +arg;
+  }, 0);
+}
+
+
+function compareArrays(arr1, arr2) {
+  return ((arr1.length === arr2.length) && arr1.every((element, index) => element === arr2[index]));
+}
+
+
+function memorize(fn, limit) {
+  return function (...args) {
+    const memory = [];
+    const find = memory.find((element) => compareArrays(element.args, args));
+    if (find) {
+      return find.result;
+    }
+    memory.unshift({args: args, result: fn(...args)});
+    if (memory.length > limit) {
+      memory.pop();
+    }
+    return memory[0].result;
+  }
+}
